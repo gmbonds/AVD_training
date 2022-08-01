e5753862dbce8b674bf6c97f08e557cd2b9deaf7
arista <atd-help@arista.com>
2022-08-01 20:22:04 +0000

    Lab 2

--- DAY1/lab2/example.py
+++ DAY1/lab2/example.py
@@ -34,16 +34,25 @@ if __name__ == '__main__':
     device_outputs = {}
 
     pp = PrettyPrinter()
-    env = Environment(loader=FileSystemLoader("/home/coder/project/labfiles/day1/lab2/"))
+    env = Environment(loader=FileSystemLoader("/home/coder/project/LABFILES/DAY1/lab2"))
     template = env.get_template("example.j2")
 
     for device in DEVICE_IPS:
         r = requests.post('https://{}:443/command-api'.format(device), json=payload, auth=(USERNAME, PASSWORD), verify=False)
         response = r.json()
-        #pp.pprint(response)
+        result = response["result"]
 
-        device_outputs[response['result'][1]['hostname']] = {'serial': response['result'][0]['serialNumber']}
+        showVersion = result[0]
+        showHostname = result[1]
+        showIpArp = result[2]
+        #['show version','show hostname','show ip arp']
+        # device_outputs[hostname] = object{'serial':serial, 'arpTable': []}
+        
+        device_outputs[showHostname['hostname']] = {'serial': showVersion['serialNumber'], 'ARP': [(entry["address"], entry["hwAddress"], entry["interface"]) for entry in showIpArp["ipV4Neighbors"]]}
         # figure out how to store the ARP address table data in a way that will make your template rendering easy
     else:
-        pp.pprint(device_outputs)
+        #print(device_outputs)
+        #pp.pprint(device_outputs)
         print(template.render(devices=device_outputs))
+        pass
+    
